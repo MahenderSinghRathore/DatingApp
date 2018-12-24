89ccdf55d9085587c0e20344767d84d5718a05ad
MahenderSinghRathore <mahiloganx@gmail.com>
2018-12-24 12:43:40 +0530

    Security Initial Done

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage="Password should be minimum 4 and maximum 8 in length")]
+        public string Password { get; set; }
+    }
+}
